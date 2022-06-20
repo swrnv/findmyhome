1f6844fd1b5e4b7c1e0d1e0a0b61760317ce7cbd
swrnv <swarnava199ghalder@gmail.com>
2022-06-20 23:23:07 +0530

    hola

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,48 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-const Banner = ({purpose}) => (
+const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt ="banner for findmyhome" />
     <Box p="5">
   <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
   <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
-  <Text color="gray.500" fontSize="sm" fontWeight="medium"></Text>
+  <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br/>{desc2}</Text>
+  <Button fontSize="xl">
+  <Link href={linkName}>{buttonText}</Link>
+  </Button>
+    
     </Box>
   </Flex>
 )
 
 export default function Home() {
   return (
-    <div>
-      <h1>testing</h1>
-      <Banner purpose={'For sale'} />
-      <Banner purpose={'For rent'} />
-    </div>
+    <Box>
+      <h1>FindMyHome</h1>
+      <Banner 
+        purpose="RENT A HOME"
+        title1="Rental homes for"
+        title2="Everyone"
+        desc1="Explore appartments, villas, homes"
+        desc2="and more..."
+        buttonText='Explore Renting'
+        linkName='/search?purpose=for-rent'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
+      <Flex flexWrap="wrap">
+        
+      </Flex>
+      <Banner 
+        purpose="BUY A HOME"
+        title1="Find, Buy & Own your"
+        title2="Dream Home"
+        desc1="Explore appartments, villas, homes"
+        desc2="and more..."
+        buttonText='Explore Buying'
+        linkName='/search?purpose=for-sale'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
+    </Box>
   )
 }
