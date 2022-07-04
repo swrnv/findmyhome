a375add44c5030220bb0ce11ef45d2c14f21a052
swrnv <swarnava199ghalder@gmail.com>
2022-07-04 21:19:41 +0530

    added footer dtuff

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,14 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Link } from '@chakra-ui/react';
 
 
 const Footer = () => (
+    <Box>
     <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
         2022 FindMyHome™, Inc. 
     </Box>
+    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+    Made with ❤️ | <Link href='https://github.com/swrnv'>Swrnv</Link>
+</Box>
+</Box>
 )
 export default Footer;
